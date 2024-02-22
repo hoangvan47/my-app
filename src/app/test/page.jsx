c1ac2dbd3c709b1ef606bc1b9c16d7a888b11fb3
hoangvan <vanhoang15091999@gmail.com>
2024-02-22 23:08:41 +0700

    Header

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import Card2 from "@/components/Card2";
+import Header from "@/components/Header";
 const TestPage = () => {
   return (
     <div>
-      <Card2 />
+      <Header />
     </div>
   );
 };
