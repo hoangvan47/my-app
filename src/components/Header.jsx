c6cb0ada764a83b9667e138002ecdcd6f2d1ee7c
hoangvan <vanhoang15091999@gmail.com>
2024-02-22 23:27:10 +0700

    Call-api-navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 import Link from "next/link";
 const Header = () => {
+  const navbar = [
+    {
+      id: 1,
+      title: "home",
+      href: "/",
+      isActive: true,
+    },
+    {
+      id: 2,
+      title: "Courses",
+      href: "/courses",
+      isActive: false,
+    },
+    {
+      id: 3,
+      title: "Pricing",
+      href: "/pricing",
+      isActive: false,
+    },
+    {
+      id: 4,
+      title: "Reviews",
+      href: "/reviews",
+      isActive: false,
+    },
+  ];
   return (
     <div className="w-[1440px] bg-[#FFFCF4] ml-auto mr-auto ">
       <div className="w-[1170px] ml-auto mr-auto pt-11 pb-16 ">
@@ -8,16 +34,13 @@ const Header = () => {
         <div className="flex items-center justify-between">
           <img src="img/logo.svg" alt="Lesson" />
 
-          <div className="ml-auto flex items-center">
+          <div className="ml-auto flex items-center gap-10">
             <Link
               href="/"
-              className="font-semibold text-base leading-7 decoration-[#171100]"
+              className={`font-semibold text-base leading-7 decoration-[#171100]`}
             >
               Home
             </Link>
-            <div className="font-normal text-base leading-7 ml-[42px]">
-              Courses
-            </div>
           </div>
           <div>
             <button>
