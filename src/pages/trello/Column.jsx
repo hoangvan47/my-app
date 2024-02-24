6b0c4fa976e4f7d13829c6bb56879ebdc0dce3a4
Cuong Nguyen <galvinnguyen1999@gmail.com>
2024-02-24 18:20:33 +0700

    Mock Data Trello

--- src/pages/trello/Column.jsx
+++ src/pages/trello/Column.jsx
@@ -5,14 +5,25 @@ import { HiOutlineDotsHorizontal } from "react-icons/hi";
 import { IoMdAdd } from "react-icons/io";
 import { IoFileTrayFullOutline } from "react-icons/io5";
 
-const Column = () => {
+const Column = ({ title, cards }) => {
+  const alowDrop = (e) => {
+    console.log(`🚀 ~ alowDrop ~ e:`, e);
+    e.preventDefault();
+  };
+
+  const doDrop = (e) => {
+    console.log(`🚀 ~ doDrop ~ e:`, e);
+    e.preventDefault();
+  };
+
   return (
     <div className="min-w-64 max-h-[500px] bg-slate-500 rounded-lg shadow-sm p-3 text-white flex flex-col gap-4">
       <div className="flex items-center justify-between pb-2 gap-2">
         <div className="font-bold text-xl">
           <input
             type="text"
-            value={"To Do"}
+            value={title || ""}
+            onChange={(e) => console.log(e.target.value)}
             className="w-full bg-transparent focus:border-gray-400 px-2 focus:outline-none"
           />
         </div>
@@ -21,20 +32,14 @@ const Column = () => {
         </button>
       </div>
 
-      <div className="flex-1 flex flex-col gap-3 overflow-y-auto">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div
+        onDragEnd={doDrop}
+        onDrag={alowDrop}
+        className="flex-1 flex flex-col gap-3 overflow-y-auto"
+      >
+        {cards?.map((card) => (
+          <Card key={card.id} title={card.title} />
+        ))}
       </div>
 
       <div className="flex items-center justify-between gap-2">
