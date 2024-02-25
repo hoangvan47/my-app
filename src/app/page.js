23a97a6deabfb0c085fe8c3bf1870175d4aedd98
hoangvan <vanhoang15091999@gmail.com>
2024-02-25 17:20:17 +0700

    connect-section-learner

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Hero from "@/pages/home/Hero";
 import Course from "@/pages/home/Course";
 import Quote from "@/pages/home/Quote";
+import Learner from "@/pages/home/Learner";
 export default function Home() {
   return (
     <div className="mx-auto">
       <Hero />
       <Course />
       <Quote />
+      <Learner />
       <div>ABC</div>
     </div>
   );
